edb5a66fc7330c5b73e11775bb29259d33639bdc
PanKreska <pawel.cacko@wp.pl>
2025-04-06 11:02:22 +0200

    updt env

---
+++ dogether/src/components/ExploreContainer.tsx
@@ -0,0 +1,14 @@
+import './ExploreContainer.css';
+
+interface ContainerProps { }
+
+const ExploreContainer: React.FC<ContainerProps> = () => {
+  return (
+    <div id="container">
+      <strong>Are you ready for the competitions?</strong>
+      
+    </div>
+  );
+};
+
+export default ExploreContainer;
